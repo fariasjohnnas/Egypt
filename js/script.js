75fad670ccbfbec5dafe475a38ed9e7d0c6732d6
fariasjohnnas <fariasjohnnas@gmail.com>
2024-12-10 23:46:56 -0500

    t

--- js/script.js
+++ js/script.js
@@ -30,10 +30,51 @@ document.addEventListener("DOMContentLoaded", () => {
     const imageWidth = carouselImages.parentElement.clientWidth; // Usa a largura do container
     carouselImages.style.transform = `translateX(-${currentIndex * imageWidth}px)`; // Apenas desloca horizontalmente
   }
-  document.getElementById("horus-face").addEventListener('click', () => {
+
+
+  document.getElementById("horus").addEventListener('click', () => {
     const horusContent = document.getElementById('horus-content');
-    horusContent.innerHTML = 'new content';
+    horusContent.innerHTML = '<b>Horus:</b> Symbol of order, protection, and divine kingship, representing the legitimacy of the pharaoh\'s rule.';
   });
+
+  document.getElementById("ramses").addEventListener('click', () => {
+    const horusContent = document.getElementById('horus-content');
+    horusContent.innerHTML = '<b>Pharaoh Ramses:</b> Mediator between order and chaos, ensuring cosmic balance (Ma’at) and the prosperity of the kingdom.';
+  });
+
+  document.getElementById("seth").addEventListener('click', () => {
+    const horusContent = document.getElementById('horus-content');
+    horusContent.innerHTML = '<b>Seth:</b> Represents chaos and destruction but also serves as a protector of Egypt against external threats.';
+  });
+
+  document.getElementById("horus").addEventListener('click', () => {
+    const horusContent = document.getElementById('horus-content');
+    horusContent.innerHTML = '<b>Horus:</b> Symbol of order, protection, and divine kingship, representing the legitimacy of the pharaoh\'s rule.';
+  });
+
+  document.getElementById("horus").addEventListener('click', () => {
+    const horusContent = document.getElementById('horus-content');
+    horusContent.innerHTML = '<b>Horus:</b> Symbol of order, protection, and divine kingship, representing the legitimacy of the pharaoh\'s rule.';
+  });
+
+  document.getElementById("horus").addEventListener('click', () => {
+    const horusContent = document.getElementById('horus-content');
+    horusContent.innerHTML = '<b>Horus:</b> Symbol of order, protection, and divine kingship, representing the legitimacy of the pharaoh\'s rule.';
+  });
+
+  document.getElementById("horus").addEventListener('click', () => {
+    const horusContent = document.getElementById('horus-content');
+    horusContent.innerHTML = '<b>Horus:</b> Symbol of order, protection, and divine kingship, representing the legitimacy of the pharaoh\'s rule.';
+  });
+
+  document.getElementById("horus").addEventListener('click', () => {
+    const horusContent = document.getElementById('horus-content');
+    horusContent.innerHTML = '<b>Horus:</b> Symbol of order, protection, and divine kingship, representing the legitimacy of the pharaoh\'s rule.';
+  });
+
 });
+
+
+
 
 
